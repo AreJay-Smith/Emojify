b6002e446099de53479f9cc392bcc29b98519513
RJ <randalljo@gmail.com>
2017-07-17 15:38:13 -0600

    Adding classifications

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -46,11 +46,30 @@ public class Emojifier {
         // If there are no faces detected, show a Toast message
         if(faces.size() == 0) {
             Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
+        } else {
+            for (int i = 0; i < faces.size(); ++i) {
+                Face face = faces.valueAt(i);
+
+                // Log the classificiation probabilities for each face.
+                getClassifications(face);
+            }
         }
 
         // Release the detector
         detector.release();
 
+    }
+    /**
+     * Method for logging the classification probabilities.
+     *
+     * @param face The face to get the classification probabilities.
+     */
+
+    private static void getClassifications(Face face) {
+        // Log all the probabilities
+        Log.d(LOG_TAG, "getClassifications: smilingProb = " +face.getIsSmilingProbability());
+        Log.d(LOG_TAG, "getClassifications: leftEyeOpenProb = " +face.getIsLeftEyeOpenProbability());
+        Log.d(LOG_TAG, "getClassifications: rightEyeOpenProb = " +face.getIsRightEyeOpenProbability());
     }
 
 }
