2fba1f6db2c5d9f172a98960678e10ffa7f0880f
RJ <randalljo@gmail.com>
2017-07-17 14:38:35 -0600

    added emjojify class

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,56 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+/**
+ * Created by Urge_Smith on 7/17/17.
+ */
+
+public class Emojifier {
+
+    private static final String LOG_TAG = Emojifier.class.getSimpleName();
+
+    /**
+     * Method for detecting faces in a bitmap.
+     *
+     * @param context The application context.
+     * @param picture The picture in which to detect the faces.
+     */
+
+    static void detectFaces(Context context, Bitmap picture) {
+
+        // Create the face detector, diable tracking and enable classifications
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        // Build the frame
+        Frame frame = new Frame.Builder().setBitmap(picture).build();
+
+        // Detect the faces
+        // A sparse array and can have open indexes without values
+        SparseArray<Face> faces = detector.detect(frame);
+
+        // Log the number of faces
+        Log.d(LOG_TAG, "detectFaces: number of faces = " + faces.size());
+
+        // If there are no faces detected, show a Toast message
+        if(faces.size() == 0) {
+            Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
+        }
+
+        // Release the detector
+        detector.release();
+
+    }
+
+}
